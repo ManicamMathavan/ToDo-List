357fee7d1b3139bf0ddec06117f069c095368449
mathavan <mthvnm@gmail.com>
2025-05-26 16:07:13 +0200

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import route from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(route),provideHttpClient()
+    provideRouter(route)
   ]
 };
